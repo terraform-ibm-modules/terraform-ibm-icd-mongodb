5ebd35c88c30ed20c6fab62a7346a13f1b4d73fb
Rajat Agrawal <rajatatibm@gmail.com>
2023-03-06 21:20:32 +0000

    feat: fscloud

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -16,7 +16,6 @@ const resourceGroup = "geretain-test-mongo"
 
 const defaultExampleTerraformDir = "examples/default"
 const autoscalingExampleTerraformDir = "examples/autoscale"
-const completeExampleTerraformDir = "examples/complete"
 const fsCloudTerraformDir = "examples/fscloud"
 
 func TestRunMongoDBFSCloudExample(t *testing.T) {
@@ -66,39 +65,12 @@ func TestRunAutoscaleExample(t *testing.T) {
 	assert.NotNil(t, output, "Expected some output")
 }
 
-func testRunComplete(t *testing.T, version string) {
-	t.Parallel()
-
-	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
-		Testing:            t,
-		TerraformDir:       completeExampleTerraformDir,
-		Prefix:             "mongodb-comp",
-		ResourceGroup:      resourceGroup,
-		BestRegionYAMLPath: regionSelectionPath,
-		TerraformVars: map[string]interface{}{
-			"mongodb_version": version,
-		},
-	})
-
-	output, err := options.RunTestConsistency()
-	assert.Nil(t, err, "This should not have errored")
-	assert.NotNil(t, output, "Expected some output")
-}
-
-func TestRunCompleteExample(t *testing.T) {
-	t.Parallel()
-	versions := []string{"4.2", "4.4"}
-	for _, version := range versions {
-		t.Run(version, func(t *testing.T) { testRunComplete(t, version) })
-	}
-}
-
 func TestRunUpgradeExample(t *testing.T) {
 	t.Parallel()
 
 	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
 		Testing:            t,
-		TerraformDir:       completeExampleTerraformDir,
+		TerraformDir:       fsCloudTerraformDir,
 		Prefix:             "mongodb-upg",
 		ResourceGroup:      resourceGroup,
 		BestRegionYAMLPath: regionSelectionPath,
