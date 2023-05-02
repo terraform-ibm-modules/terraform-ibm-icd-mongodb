9a5fc4f10a1b93c1d93f080d4e7c9b83141267d2
rajatagarwal-ibm <108140212+rajatagarwal-ibm@users.noreply.github.com>
2023-05-02 11:38:59 +0100

    feat: added new FSCloud terraform submodule (profiles/fscloud) with example on how to consume + added support for Hyper Protect + added support for creating the KMS auth policy. `key_protect_key_crn` has been renamed `kms_key_crn` (#139)

---
+++ examples/fscloud/README.md
@@ -0,0 +1,16 @@
+# Financial Services Cloud profile example
+
+## *Note:* This example is only deploying MongoDB in a compliant manner the other infrastructure is not necessarily compliant.
+
+### Requirements
+This example expects you have Hyper Protect Crypto Service instances in the region you wish to deploy your MongoDB instance.
+
+### Deploys
+An example using the fscloud profile to deploy a compliant MongoDB instance. This example uses the IBM Cloud terraform provider to:
+
+- Create a new resource group if one is not passed in.
+- Create a Key protect instance and IAM Authorization between MongoDB instance resource group and Key Protect Instance.
+- Create an IAM Authorization between MongoDB instance Resource group and HPSC permanent Instance.
+- Create a new ICD MongoDB instance and credentials that is encrypted using the Hyper Protect Crypto Service resources that are passed in.
+- Create a Sample VPC.
+- Create Context Based Restriction(CBR) to only allow Postgresql to be accessible from the VPC.
---
+++ examples/fscloud/variables.tf
@@ -0,0 +1,39 @@
+variable "ibmcloud_api_key" {
+  type        = string
+  description = "The IBM Cloud API Key"
+  sensitive   = true
+}
+
+variable "region" {
+  type        = string
+  description = "Region to provision all resources created by this example"
+  default     = "us-south"
+}
+
+variable "prefix" {
+  type        = string
+  description = "Prefix to append to all resources created by this example"
+  default     = "fs-cloud"
+}
+
+variable "resource_group" {
+  type        = string
+  description = "An existing resource group name to use for this example, if unset a new resource group will be created"
+  default     = null
+}
+
+variable "resource_tags" {
+  type        = list(string)
+  description = "Optional list of tags to be added to created resources"
+  default     = []
+}
+
+variable "existing_kms_instance_guid" {
+  description = "The GUID of the Hyper Protect Crypto service in which the key specified in var.kms_key_crn is coming from"
+  type        = string
+}
+
+variable "kms_key_crn" {
+  type        = string
+  description = "The root key CRN of a Hyper Protect Crypto Service (HPCS) that you want to use for disk encryption. See https://cloud.ibm.com/docs/cloud-databases?topic=cloud-databases-hpcs&interface=ui for more information on integrating HPCS with MongoDB instance."
+}
