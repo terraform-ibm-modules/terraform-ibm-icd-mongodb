5e58cacb229baf9bcc8ec7c9f81c41b58f4ed1bb
akocbek@ie.ibm.com <akocbek@ie.ibm.com>
2023-08-15 16:18:59 +0100

    fix: port and hostname outputs should not be flatten

--- examples/fscloud/outputs.tf
+++ examples/fscloud/outputs.tf
@@ -15,3 +15,13 @@ output "version" {
   description = "MongoDB instance version"
   value       = module.mongodb.version
 }
+
+output "hostname" {
+  description = "Database hostname. Only contains value when var.service_credential_names or var.users are set."
+  value       = module.mongodb.hostname
+}
+
+output "port" {
+  description = "Database port. Only contains value when var.service_credential_names or var.users are set."
+  value       = module.mongodb.port
+}
