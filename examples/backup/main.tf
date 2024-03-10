d43cf6a781f541dc40ac364a5b9b16c320c42dfe
Akash Kumar <akash.kumar@ibm.com>
2024-03-10 15:13:44 +0530

    small fixes

--- examples/backup/main.tf
+++ examples/backup/main.tf
@@ -34,7 +34,7 @@ data "ibm_database_backups" "backup_database" {
 module "restored_mongo_db" {
   source            = "../.."
   resource_group_id = module.resource_group.resource_group_id
-  instance_name     = "${var.prefix}-mongodb"
+  instance_name     = "${var.prefix}-mongodb-restored"
   region            = var.region
   mongodb_version   = var.mongodb_version
   access_tags       = var.access_tags
