08f8bab6af49e4a0bdf20c6d4e74404dfd423fe8
Steve Peggs <peggs@uk.ibm.com>
2024-07-08 13:00:09 +0100

    fix(deps): bump IBM provider

--- modules/fscloud/version.tf
+++ modules/fscloud/version.tf
@@ -8,7 +8,7 @@ terraform {
     ibm = {
       source = "IBM-Cloud/ibm"
       # Use "greater than or equal to" range in modules
-      version = ">=1.64.2, <2.0.0"
+      version = ">=1.65.0, <2.0.0"
     }
   }
 }
