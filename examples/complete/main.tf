7cf6cdc570d345bad4e3782d655a2b1f4f4ff6fa
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-08-09 16:13:04 +0000

    fix(deps): update dependencies

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -53,7 +53,7 @@ locals {
 
 module "key_protect_all_inclusive" {
   source            = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version           = "5.1.18"
+  version           = "5.1.19"
   resource_group_id = module.resource_group.resource_group_id
   # Only us-south, us-east and eu-de backup encryption keys are supported. See https://cloud.ibm.com/docs/cloud-databases?topic=cloud-databases-key-protect&interface=ui#key-byok for details.
   # Note: Database instance and Key Protect must be created on the same region.
@@ -137,7 +137,7 @@ module "icd_mongodb" {
 module "secrets_manager" {
   count                = var.existing_secrets_manager_instance_guid == null ? 1 : 0
   source               = "terraform-ibm-modules/secrets-manager/ibm"
-  version              = "2.7.2"
+  version              = "2.7.3"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   secrets_manager_name = "${var.prefix}-secrets-manager"
