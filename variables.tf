4dde83794e8a97da9306ab46595864a977af143a
Khuzaima-Shakeel <Khuzaima.Shakeel@ibm.com>
2025-09-16 16:23:53 +0530

    added new version

--- variables.tf
+++ variables.tf
@@ -21,8 +21,9 @@ variable "mongodb_version" {
     condition = anytrue([
       var.mongodb_version == null,
       var.mongodb_version == "8.0",
+      var.mongodb_version == "7.0",
     ])
-    error_message = "Version must be either 8.0. If no value is passed, the current preferred version of IBM Cloud Databases is used."
+    error_message = "Version must be either 8.0 or 7.0. If no value is passed, the current preferred version of IBM Cloud Databases is used."
   }
 }
 
