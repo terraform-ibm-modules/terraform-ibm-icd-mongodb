4ff8b524c32b74c03db727e1f0918c152c28c57b
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2025-11-11 20:17:55 +0000

    Update variables.tf

--- solutions/security-enforced/variables.tf
+++ solutions/security-enforced/variables.tf
@@ -10,33 +10,33 @@ variable "ibmcloud_api_key" {
 
 variable "existing_resource_group_name" {
   type        = string
-  description = "The name of an existing resource group to provision the resources. If not provided the default resource group will be used."
-  default     = null
+  description = "The name of an existing resource group to provision the resources. [Learn more](https://cloud.ibm.com/docs/account?topic=account-rgs&interface=ui#create_rgs) about how to create a resource group."
+  default     = "Default"
 }
 
 variable "prefix" {
   type        = string
   nullable    = true
-  description = "The prefix to be added to all resources created by this solution. To skip using a prefix, set this value to null or an empty string. The prefix must begin with a lowercase letter and may contain only lowercase letters, digits, and hyphens '-'. It should not exceed 16 characters, must not end with a hyphen('-'), and can not contain consecutive hyphens ('--'). Example: prod-0205-cos. [Learn more](https://terraform-ibm-modules.github.io/documentation/#/prefix.md)."
+  description = "The prefix to add to all resources that this solution creates (e.g `prod`, `test`, `dev`). To skip using a prefix, set this value to `null` or an empty string. [Learn more](https://terraform-ibm-modules.github.io/documentation/#/prefix.md)."
 
   validation {
-    # - null and empty string is allowed
-    # - Must not contain consecutive hyphens (--): length(regexall("--", var.prefix)) == 0
-    # - Starts with a lowercase letter: [a-z]
-    # - Contains only lowercase letters (a–z), digits (0–9), and hyphens (-)
-    # - Must not end with a hyphen (-): [a-z0-9]
-    condition = (var.prefix == null || var.prefix == "" ? true :
-      alltrue([
-        can(regex("^[a-z][-a-z0-9]*[a-z0-9]$", var.prefix)),
-        length(regexall("--", var.prefix)) == 0
-      ])
-    )
-    error_message = "Prefix must begin with a lowercase letter and may contain only lowercase letters, digits, and hyphens '-'. It must not end with a hyphen('-'), and cannot contain consecutive hyphens ('--')."
-  }
-
+  # - null and empty string is allowed
+  # - Must not contain consecutive hyphens (--): length(regexall("--", var.prefix)) == 0
+  # - Starts with a lowercase letter: [a-z]
+  # - Contains only lowercase letters (a–z), digits (0–9), and hyphens (-) 
+  # - Must not end with a hyphen (-): [a-z0-9]
+  condition = (var.prefix == null || var.prefix == "" ? true :
+    alltrue([
+      can(regex("^[a-z][-a-z0-9]*[a-z0-9]$", var.prefix)),
+      length(regexall("--", var.prefix)) == 0
+    ])
+  )
+  error_message = "Prefix must begin with a lowercase letter and may contain only lowercase letters, digits, and hyphens '-'. It must not end with a hyphen('-'), and cannot contain consecutive hyphens ('--')."
+ }
+ 
   validation {
-    # must not exceed 16 characters in length
-    condition     = var.prefix == null || var.prefix == "" ? true : length(var.prefix) <= 16
+   # must not exceed 16 characters in length
+    condition = var.prefix == null || var.prefix == "" ? true : length(var.prefix) <= 16
     error_message = "Prefix must not exceed 16 characters."
   }
 }
