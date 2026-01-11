2de7dc127f79003517296ad8979a639050fa31df
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-01-11 04:14:52 +0000

    fix(deps): update terraform terraform-ibm-modules/secrets-manager/ibm to v2.12.18 (#705)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -137,7 +137,7 @@ module "icd_mongodb" {
 module "secrets_manager" {
   count                = var.existing_secrets_manager_instance_guid == null ? 1 : 0
   source               = "terraform-ibm-modules/secrets-manager/ibm"
-  version              = "2.12.17"
+  version              = "2.12.18"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   secrets_manager_name = "${var.prefix}-secrets-manager"
