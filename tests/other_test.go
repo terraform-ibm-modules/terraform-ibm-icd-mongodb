f8e13fc2b51f792321dce7f4c4daf3e664318b2a
Vipin Kumar <kumarvipin88073@gmail.com>
2026-01-20 23:30:43 +0530

    removed plan test

--- tests/other_test.go
+++ tests/other_test.go
@@ -13,32 +13,6 @@ import (
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
 )
 
-func testPlanICDVersions(t *testing.T, version string) {
-	t.Parallel()
-
-	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
-		Testing:      t,
-		TerraformDir: "examples/basic",
-		TerraformVars: map[string]interface{}{
-			"mongodb_version": version,
-		},
-		CloudInfoService: sharedInfoSvc,
-	})
-	output, err := options.RunTestPlan()
-	assert.Nil(t, err, "This should not have errored")
-	assert.NotNil(t, output, "Expected some output")
-}
-
-func TestPlanICDVersions(t *testing.T) {
-	t.Parallel()
-
-	// This test will run a terraform plan on available stable versions of mongodb
-	versions, _ := sharedInfoSvc.GetAvailableIcdVersions("mongodb")
-	for _, version := range versions {
-		t.Run(version, func(t *testing.T) { testPlanICDVersions(t, version) })
-	}
-}
-
 func TestRunRestoredDBExample(t *testing.T) {
 	t.Parallel()
 
@@ -69,7 +43,7 @@ func TestRunCompleteExample(t *testing.T) {
 		BestRegionYAMLPath: regionSelectionPath,
 		ResourceGroup:      resourceGroup,
 		TerraformVars: map[string]interface{}{
-			"mongodb_version": oldestVersion, // Always lock to the lowest supported MongoDB version
+			"mongodb_version": latestVersion,
 			"plan":            "standard",
 			"users": []map[string]interface{}{
 				{
