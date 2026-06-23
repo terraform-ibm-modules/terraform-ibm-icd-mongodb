ef2b043cababed09733da915170a83697ff02372
Vipin Kumar <77929205+vkuma17@users.noreply.github.com>
2026-06-23 10:55:06 +0530

    fix: bump `common-utilities` version to support Gen2 database validation  (#779)
    * fix: new common utilities version for gen2 version validation
    
    * Update main.tf
    
    * secrets baseline fix

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -46,21 +46,7 @@ var validICDRegions = []string{
 	"us-south",
 }
 
-func GetRegionVersions(region string) (string, string) {
-
-	cloudInfoSvc, err := cloudinfo.NewCloudInfoServiceFromEnv("TF_VAR_ibmcloud_api_key", cloudinfo.CloudInfoServiceOptions{
-		IcdRegion: region,
-	})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	icdAvailableVersions, err := cloudInfoSvc.GetAvailableIcdVersions(icdType)
-
-	if err != nil {
-		log.Fatal(err)
-	}
+func GetLatestAndOldestVersions(icdAvailableVersions []string) (string, string) {
 
 	if len(icdAvailableVersions) == 0 {
 		log.Fatal("No available ICD versions found")
@@ -94,10 +80,55 @@ func GetRegionVersions(region string) (string, string) {
 	oldestVersion := icdAvailableVersions[0]
 
 	return latestVersion, oldestVersion
+
+}
+
+func GetRegionVersions(region string) (string, string) {
+
+	icdRegion := region
+	if region == "ca-mon" {
+		icdRegion = "ca-tor"
+	}
+
+	cloudInfoSvc, err := cloudinfo.NewCloudInfoServiceFromEnv("TF_VAR_ibmcloud_api_key", cloudinfo.CloudInfoServiceOptions{
+		IcdRegion: icdRegion,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	icdAvailableVersions, err := cloudInfoSvc.GetAvailableIcdVersions(icdType)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return GetLatestAndOldestVersions(icdAvailableVersions)
+}
+
+func GetVersionsGen2(region string, plan string) (string, string) {
+
+	cloudInfoSvc, err := cloudinfo.NewCloudInfoServiceFromEnv("TF_VAR_ibmcloud_api_key", cloudinfo.CloudInfoServiceOptions{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	icdAvailableVersions, err := cloudInfoSvc.GetAvailableIcdVersionsGen2("databases-for-mongodb", plan, region) // this function takes service, plan and region as arguments in this specific order
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return GetLatestAndOldestVersions(icdAvailableVersions)
 }
 
 func TestRunBasicGen2Example(t *testing.T) {
 	t.Parallel()
+
+	latestVersion, _ := GetVersionsGen2("ca-mon", "standard-gen2")
+	fmt.Println("latestVersion is ", latestVersion)
 
 	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
 		Testing:            t,
@@ -108,7 +139,7 @@ func TestRunBasicGen2Example(t *testing.T) {
 		TerraformVars: map[string]interface{}{ // Limited gen2 to Montreal and version 18
 			"region":            "ca-mon",
 			"plan":              "standard-gen2",
-			"mongodb_version":   "8.0",
+			"mongodb_version":   latestVersion,
 			"service_endpoints": "private",
 		},
 		CloudInfoService: sharedInfoSvc,
