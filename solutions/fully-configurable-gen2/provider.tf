6ad1fd4419b6a3659a21b11d63a2d6f22d62f655
Ayushmaan Singh <Ayushmaan.Singh1@ibm.com>
2026-06-30 10:38:03 +0530

    fix: added VPE support for private endpoints in ca-mon

--- solutions/fully-configurable-gen2/provider.tf
+++ solutions/fully-configurable-gen2/provider.tf
@@ -2,6 +2,7 @@ provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
   region           = var.region
   visibility       = var.provider_visibility
+  private_endpoint_type = (var.provider_visibility == "private" && var.region == "ca-mon") ? "vpe" : null
 }
 
 # Provider block for KMS (Key Protect or Key Protect Dedicated)
@@ -10,4 +11,5 @@ provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_kms_api_key != null ? var.ibmcloud_kms_api_key : var.ibmcloud_api_key
   region           = var.region
   visibility       = var.provider_visibility
+  private_endpoint_type = (var.provider_visibility == "private" && var.region == "ca-mon") ? "vpe" : null
 }
