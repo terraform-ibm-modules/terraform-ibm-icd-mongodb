981cd495a703d8631f6034cf9ece9beeab5cc94d
Aayush-Abhyarthi <Aayush.Abhyarthi@ibm.com>
2026-08-12 13:44:21 +0530

    resolve comment

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -294,6 +294,7 @@ func TestRunFullyConfigurableGen2SolutionSchematics(t *testing.T) {
 		{Name: "existing_resource_group_name", Value: uniqueResourceGroup, DataType: "string"},
 		{Name: "region", Value: "ca-mon", DataType: "string"},
 		{Name: "plan", Value: "enterprise-sharding-gen2", DataType: "string"},
+		{Name: "member_host_flavor", Value: "bx3d.8x40", DataType: "string"},
 		{Name: "service_credential_names", Value: serviceCredentialNames, DataType: "list(object)"},
 		{Name: "service_credential_secrets", Value: serviceCredentialSecrets, DataType: "list(object)"},
 		{Name: "existing_secrets_manager_instance_crn", Value: permanentResources["secretsManagerCRN"], DataType: "string"},
