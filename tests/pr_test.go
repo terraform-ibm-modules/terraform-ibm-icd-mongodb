e02fbbb900f300da34adf5f4c93bc044ab3569ba
Aayush-Abhyarthi <Aayush.Abhyarthi@ibm.com>
2026-09-07 17:24:58 +0530

    add test with hardcoded backup crn

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -524,3 +524,24 @@ func generateUniqueResourceGroupName(baseName string) string {
 	id := uuid.New().String()[:8] // Shorten UUID for readability
 	return fmt.Sprintf("%s-%s", baseName, id)
 }
+
+func TestRunRestoredDBGen2Example(t *testing.T) {
+	t.Parallel()
+
+	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
+		Testing:       t,
+		TerraformDir:  "examples/backup-restore",
+		Prefix:        "g2-restored",
+		Region:        "eu-de", // Hardcode the region of permanent resource
+		ResourceGroup: resourceGroup,
+		TerraformVars: map[string]interface{}{
+			"plan":       "standard-gen2",
+			"backup_crn": "crn:v1:bluemix:public:databases-for-mongodb:eu-de:a/abac0df06b644a9cabc6e44f55b3880e:0d459b7a-2896-444a-99a7-5bade08186fd:backup:ded08ca3-7b1e-4ee6-95ae-9f1fcaa68cf3",
+		},
+		CloudInfoService: sharedInfoSvc,
+	})
+
+	output, err := options.RunTestConsistency()
+	assert.Nil(t, err, "This should not have errored")
+	assert.NotNil(t, output, "Expected some output")
+}
