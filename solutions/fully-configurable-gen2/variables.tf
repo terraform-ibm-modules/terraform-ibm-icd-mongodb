22d08f2ba1b207259987d23fc0cd80e414873f57
zikra-iqbal <zikra.iqbalmnnit@gmail.com>
2026-09-23 01:35:38 +0530

    fix: Add new skip variable for independent backups

--- solutions/fully-configurable-gen2/variables.tf
+++ solutions/fully-configurable-gen2/variables.tf
@@ -232,7 +232,13 @@ variable "existing_kms_key_crn" {
 
 variable "skip_mongodb_kms_auth_policy" {
   type        = bool
-  description = "Whether to create an IAM authorization policy that permits all Databases for MongoDB instances in the resource group to read the encryption key from the instance specified in the `existing_kms_instance_crn` variable."
+  description = "Whether to skip the creation of IAM authorization policies for the MongoDB instance. When set to false (default) and `kms_encryption_enabled` is true, a policy that permits all Databases for MongoDB instances in the resource group 'Reader' access to the encryption key from the Key Protect instance specified in the `existing_kms_instance_crn` variable (required for KMS encryption) is created. Skip only if one already exists in your account)."
+  default     = false
+}
+
+variable "skip_independent_backup_policies" {
+  type        = bool
+  description = "Set to true to skip the creation of independent backup authorization policies. When set to false (default). The following 2 policies will be created - (1) a policy that permits Databases for MongoDB instances in the given resource group 'Editor' access to the independent backups service (`gen2_independent_backups_policy`), (2) a policy that permits Databases for MongoDB instances in the given resource group 'Viewer' access to the resource group (`gen2_resource_group_policy`)."
   default     = false
 }
 
